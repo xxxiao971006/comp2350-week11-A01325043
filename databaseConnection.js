5a59dde17f704e09b183f3666ba980978a939983
xxxiao971006 <114138744+xxxiao971006@users.noreply.github.com>
2023-03-24 16:23:30 -0700

    Update databaseConnection.js

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,6 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 const is_qoddi = process.env.IS_QODDI || false;
-const qoddiURI ="mongodb+srv://xiao:Zxx53625140%40@cluster0.h5qplzn.mongodb.net/?retryWrites=true&w=majority"
+const qoddiURI ="mongodb+srv://xiao@cluster0.h5qplzn.mongodb.net/admin?retryWrites=true&replicaSet=atlas-pofeot-shard-0&readPreference=primary&srvServiceName=mongodb&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1&3t.uriVersion=3&3t.connection.name=atlas-pofeot-shard-0&3t.databases=admin&3t.alwaysShowAuthDB=true&3t.alwaysShowDBFromUserRole=true&3t.sslTlsVersion=TLS"
 const localURI = "mongodb://127.0.0.1:27017/?retryWrites=true&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&3t.uriVersion=3&3t.connection.name=localhost&3t.alwaysShowAuthDB=true&3t.alwaysShowDBFromUserRole=true";
 if (is_qoddi) {
 	var database = new MongoClient(qoddiURI,
